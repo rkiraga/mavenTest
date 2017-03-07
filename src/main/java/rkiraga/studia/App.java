2d0b86dbf58e57e871f15e8abaa443cea055e13c
Radek2 <radek69@vp.pl>
2017-03-07 19:15:26 +0100

    xml sec

--- src/main/java/rkiraga/studia/App.java
+++ src/main/java/rkiraga/studia/App.java
@@ -1,13 +1,27 @@
 package rkiraga.studia;
 
+
+import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
+import com.sun.org.apache.xml.internal.security.utils.Base64;
+
 /**
  * Hello world!
  *
  */
+
+
 public class App 
 {
     public static void main( String[] args )
     {
+        com.sun.org.apache.xml.internal.security.Init.init();
         System.out.println( "Hello World!" );
+        try {
+            byte[] tab = Base64.decode("cXdlcnR5".getBytes());
+            System.out.println(new String(tab));
+        } catch (Base64DecodingException e) {
+            e.printStackTrace();
+        }
+
     }
 }
